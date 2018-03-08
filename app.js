1ecf1fb7c48a836bfa57d1b5f90012500779dc4e
Molly Kent <kent.molly@gmail.com>
2018-03-07 16:44:46 -0800

    complete tooltip functionality

--- app.js
+++ app.js
@@ -1,18 +1,131 @@
 (function() {
+
+  var colorIndex = {
+    Burst: "#fab899",
+    Apricot: "#f6947e",
+    Mango: "#f69266",
+    Mesa: "#f37645",
+    Orange: "#f7951e",
+    Kumquat: "#f16035",
+    Candy: "#f9c3c4",
+    Flamingo: "#f16269",
+    Sunset: "#f05146",
+    Autumn: "#ee2736",
+    Tropical: "#c1196f",
+    Pomegranate: "#983261",
+    Chocolate: "#57382b",
+    Canyon: "#663629",
+    Oak: "#6a5141",
+
+    Tweed: "#937c67",
+    Brown: "#8c5e2a",
+    Camel: "#ce9c72",
+    Cherry: "#761532",
+    Clay: "#855853",
+    Thistle: "#b285a2",
+    Petunia: "#a95ea2",
+    Tart: "#906794",
+    Cosmic_Blue: "#39536b",
+    Freedom_Blue: "#2b375b",
+    Denim: "#44749f",
+    Mineral: "#035179",
+    Royal: "#204294",
+    Velvet: "#166ba9",
+    Seaweed: "#0086ae",
+
+    Terra: "#4e453a",
+    Evergreen: "#394f38",
+    Rugby: "#255f4a",
+    Fern: "#017f51",
+    Garden: "#7ab87a",
+    Meadow: "#4fbc7e",
+    Olive: "#84a265",
+    Citron: "#d0e5a6",
+    Neon: "#aed9a8",
+    Sap: "#b1c887",
+    Chartreuse: "#bbc85e",
+    Solar: "#efca40",
+    Spark_Gold: "#c59c30",
+    Saffron: "#fec037",
+    Lemon: "#fbee78",
+
+    Reef: "#35a9d1",   
+    Cadet: "#6ba2c1",   
+    Serpent: "#0e575f",   
+    Baltic: "#007886",   
+    Malachite: "#709ba1",   
+    Jungle: "#44816b",   
+    Jade: "#009e94",   
+    Macaw: "#3abfb1",   
+    Cactus: "#70c295",   
+    Light_Jade: "#6cc8ba",   
+    Sky: "#5ac0de",   
+    Cloud: "#a1dcf1",   
+    Caribbean_Sea: "#a9ddd7",   
+    Asparagus: "#9baf94",   
+    Sycamore: "#bda98e",   
+
+    Salt: "#cabc95",
+    Chamois: "#f2ebbf",
+    Mist: "#fcfbf2",
+    Shell: "#f7edd3",
+    Silver: "#aeb7bd",
+    Sand_Dune: "#ccc0b9",
+    Dogwood: "#afa99e",
+    Desert: "#97827d",
+    Brass: "#a39986",
+    Shadow: "#8c918f",
+    Slate_gray: "#6f7579",
+    Manatee: "#515659",
+    Licorice: "#1d1d23",
+    Vino: "#4e2130",
+    Chona_Brown: "#46332b"
+}
+
+
   
   var chosenColor = "#fcfbf2";
-  var fillColor;
-  var position;
+  var fillColorId;
+  var fillColorName;
+  var parentClasses;
+  var pieceClasses;
+  var pieceId;
+  var showColors;
+
+  function getKeyByValue(object, value) {
+    return Object.keys(object).find(key => object[key] === value);
+  }
+
+  $("#enableTooltips").on("change", function() {
+    if($("#enableTooltips").is(':checked')) {
+    showColors = true;  // checked
+  } else {
+    showColors = false;
+  } 
+  })
+
 
   $(".piece").hover(function() {
-    fillColor = $(this).attr("fill");
-    position = $(this).attr("class");
-    console.log(position);
-    $(this).attr("fill", "#000");
+    if (showColors) {
+      fillColorId = $(this).attr("fill");
+      fillColorName = getKeyByValue(colorIndex, fillColorId);
+      parentClasses = $(this).parent().attr("class");
+      parentClasses = parentClasses.replace(/\s+/g, '');
+      pieceClasses = $(this).attr("class");
+      pieceClasses = pieceClasses.replace("piece ", "");
+      pieceId = parentClasses + pieceClasses;
+  
+      $("#"+pieceId+"").text('');
+      $("#"+pieceId+"").text(fillColorName);
+      $("#"+pieceId+"").removeClass('hidden');
+    }
   }, function() {
-    $(this).attr("fill", fillColor);
-  }
-);
+    if (showColors){
+    $("#"+pieceId+"").addClass('hidden');
+    }
+  });
+
+
 
   $(".colorChoice").on("click", function() {
     var chosenLabel = '';
